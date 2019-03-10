55860274876894a9600ef8324d89e30734c851b0
Richard Steinmetz <steinmetz.richard@googlemail.com>
2019-03-10 13:41:07 +0100

    Improve fuse cli

--- src/stzfs.c
+++ src/stzfs.c
@@ -1,21 +1,47 @@
 #define FUSE_USE_VERSION 34
 #include <fuse3/fuse.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "syscalls.h"
 #include "vm.h"
 
+void print_usage(void) {
+    printf("usage: stzfs <device> <dir>\n");
+}
+
 int main(int argc, char** argv) {
-	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-
-	// parse options
-    /*
-	if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1) {
-		return 1;
+    if (argc < 3) {
+        print_usage();
+        return 1;
     }
-    */
 
-    vm_config_set_file("/tmp/vm-hdd.dat");
-	int ret = fuse_main(args.argc, args.argv, &sys_ops, NULL);
-	fuse_opt_free_args(&args);
-	return ret;
+    // extract device
+    char* device = NULL;
+    char** argv_new = (char**)malloc(argc * sizeof(char**));
+    argv_new[0] = argv[0];
+    int index = 1;
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] != '-' && device == NULL) {
+            device = argv[i];
+            continue;
+        }
+        argv_new[index] = argv[i];
+        index++;
+    }
+
+    if (device == NULL) {
+        print_usage();
+        return 0;
+    }
+
+    // run fuse
+    printf("mounting %s\n", device);
+    vm_config_set_file(device);
+    struct fuse_args args = FUSE_ARGS_INIT(index, argv_new);
+    int ret = fuse_main(args.argc, args.argv, &sys_ops, NULL);
+
+    // cleanup fuse
+    fuse_opt_free_args(&args);
+    return ret;
 }
