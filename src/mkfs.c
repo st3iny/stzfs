b31dda6d1dc40704d385df2ca3877b2f43b87f1c
Richard Steinmetz <steinmetz.richard@googlemail.com>
2019-03-24 22:57:19 +0100

    Improve mkfs cli

--- src/mkfs.c
+++ src/mkfs.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "blocks.h"
 #include "syscalls.h"
+#include "types.h"
 #include "vm.h"
 
 int main(int argc, char** argv) {
-    if (argc <= 1) {
-        printf("usage: mkfs.stzfs <device>\n");
+    if (argc < 2 || argc > 3) {
+        printf("usage: mkfs.stzfs <device> [inode_count]\n");
         return 1;
     }
 
-    vm_config_set_file(argv[1]);
-    stzfs_makefs(1048576);
+    off_t size = vm_config_set_file(argv[1]);
+
+    inodeptr_t inode_count;
+    if (argc == 3) {
+        inode_count = strtol(argv[2], NULL, 10);
+    } else {
+        inode_count = (size / sizeof(inode_t)) / 8;
+    }
+    stzfs_makefs(inode_count);
 
     return 0;
 }
